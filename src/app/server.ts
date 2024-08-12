1793897d1e6b160cce7aac0038627783b159fa85
jardmclan <mcleanj@hawaii.edu>
2024-08-12 13:17:16 -1000

    debug

--- src/app/server.ts
+++ src/app/server.ts
@@ -218,10 +218,10 @@ function validateToken(req, permission) {
 
 async function sendEmail(transporterOptions, mailOptions) {
   console.log("sendEmail");
-  let combinedMailOptions = Object.assign({debug: true, logger: true}, mailOptionsBase, mailOptions);
-
+  let combinedMailOptions = Object.assign({debug: true, logger: true, tls: {ciphers: "ECDHE-RSA-AES256-GCM-SHA384"}}, mailOptionsBase, mailOptions);
+  console.log("creating transport");
   let transporter = nodemailer.createTransport(transporterOptions);
-
+  console.log("transport created");
   //have to be on uh netork
   return transporter.sendMail(combinedMailOptions)
   .then((info) => {
