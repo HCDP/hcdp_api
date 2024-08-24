9ccfd1a3ef5ef1f5a3f6202d0986bf21cf5b692d
jardmclan <mcleanj@hawaii.edu>
2024-08-23 16:17:56 -1000

    merge and remove debug

--- src/app/server.ts
+++ src/app/server.ts
@@ -596,7 +596,6 @@ app.get("/raster", async (req, res) => {
       ...properties
     }];
     let files = await getPaths(productionRoot, data, false);
-    console.log(files);
     reqData.sizeF = files.numFiles;
     let file = "";
     //should only be exactly one file
