ff8fdf4581cd5129293a14da169096c72eeb64aa
jardmclan <mcleanj@hawaii.edu>
2024-10-03 15:57:22 -1000

    allow date range in raw list

--- src/app/server.ts
+++ src/app/server.ts
@@ -1148,9 +1148,14 @@ app.get("/raw/sff", async (req, res) => {
 app.get("/raw/list", async (req, res) => {
   const permission = "basic";
   await handleReq(req, res, permission, async (reqData) => {
-    let { date, station_id, location }: any = req.query;
+    let { date, startDate, endDate, station_id, location }: any = req.query;
 
-    if(!date) {
+    if(date) {
+      startDate = date;
+      endDate = date;
+    }
+
+    if(!(startDate && endDate)) {
       //set failure and code in status
       reqData.success = false;
       reqData.code = 400;
@@ -1159,6 +1164,8 @@ app.get("/raw/list", async (req, res) => {
       .send(
         "Request must include the following parameters: \n\
         date: An ISO 8601 formatted date string representing the date you would like the data for. \n\
+        startDate: An ISO 8601 formatted date string representing the first date you would like the data for. \n\
+        endDate: An ISO 8601 formatted date string representing the last date you would like the data for. \n\
         station_id (optional): The station ID you want to get files for. \n\
         location (optional): The sensor network location to retrieve files for. Default hawaii"
       );
@@ -1167,41 +1174,43 @@ app.get("/raw/list", async (req, res) => {
       if(location === undefined) {
         location = "hawaii"
       }
-      let parsedDate = moment(date);
-      let year = parsedDate.format("YYYY");
-      let month = parsedDate.format("MM");
-      let day = parsedDate.format("DD");
-  
-      let dataDir = path.join(location, year, month, day);
-      let sysDir = path.join(rawDataRoot, dataDir);
-      let linkDir = `${apiURL}/raw/download?p=${dataDir}/`;
-  
-      let { err, files } = await readdir(sysDir);
-
-      //no dir for requested date, just return empty
-      if(err && err.code == "ENOENT") {
-        files = [];
-      }
-      else if(err) {
-        throw err;
-      }
-
-      //if a station ID was specified filter files by ones starting with that id
-      if(station_id !== undefined) {
-        files = files.filter((file) => {
-          let fid = file.split("_")[0];
-          return fid == station_id;
+      let allFiles = [];
+      let parsedDate = moment(startDate);
+      let parsedEndDate = moment(endDate);
+      while(parsedDate.isSameOrBefore(parsedEndDate)) {
+        parsedDate.add(1, "day");
+        let year = parsedDate.format("YYYY");
+        let month = parsedDate.format("MM");
+        let day = parsedDate.format("DD");
+        let dataDir = path.join(location, year, month, day);
+        let sysDir = path.join(rawDataRoot, dataDir);
+        let linkDir = `${apiURL}/raw/download?p=${dataDir}/`;
+        let { err, files } = await readdir(sysDir);
+        //no dir for requested date, just return empty
+        if(err && err.code == "ENOENT") {
+          files = [];
+        }
+        else if(err) {
+          throw err;
+        }
+        //if a station ID was specified filter files by ones starting with that id
+        if(station_id !== undefined) {
+          files = files.filter((file) => {
+            let fid = file.split("_")[0];
+            return fid == station_id;
+          });
+        }
+        files = files.map((file) => {
+          let fileLink = `${linkDir}${file}`;
+          return fileLink;
         });
+        allFiles = allFiles.concat(files);
       }
-  
-      files = files.map((file) => {
-        let fileLink = `${linkDir}${file}`;
-        return fileLink;
-      });
-      reqData.sizeF = files.length;
+      
+      reqData.sizeF = allFiles.length;
       reqData.code = 200;
       res.status(200)
-      .json(files);
+      .json(allFiles);
     }
   });
 });
