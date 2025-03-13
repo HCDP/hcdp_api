a9bb8873a8ff203bd60b0295b596601240c68a98
jardmclan <mcleanj@hawaii.edu>
2025-03-12 18:01:33 -1000

    add file explorer ep

--- src/app/endpoints/hcdp/misc/misc.ts
+++ src/app/endpoints/hcdp/misc/misc.ts
@@ -1,13 +1,14 @@
 import express from "express";
-import { handleReq } from "../../../modules/util/reqHandlers.js";
+import { handleReq, handleReqNoAuth } from "../../../modules/util/reqHandlers.js";
 import { tapisManager } from "../../../modules/util/resourceManagers/tapis.js";
 import { processTapisError, handleSubprocess } from "../../../modules/util/util.js";
 import { getPaths, fnamePattern, getEmpty } from "../../../modules/fileIndexer.js";
-import { urlRoot, dataRoot } from "../../../modules/util/config.js";
+import { urlRoot, dataRoot, apiURL } from "../../../modules/util/config.js";
 import * as child_process from "child_process";
 import * as path from "path";
 import * as fs from "fs";
 import * as crypto from "crypto";
+import * as url from "url";
 
 export const router = express.Router();
 
@@ -129,32 +130,32 @@ router.get("/raster/timeseries", async (req, res) => {
 router.get("/raster", async (req, res) => {
   const permission = "basic";
   await handleReq(req, res, permission, async (reqData) => {
-      //destructure query
-      let {date, returnEmptyNotFound, type, ...properties} = req.query;
-      if(type === undefined) {
+    //destructure query
+    let {date, returnEmptyNotFound, type, ...properties} = req.query;
+    if(type === undefined) {
       type = "data_map";
-      }
+    }
 
-      let data = [{
+    let data = [{
       files: [type],
       range: {
           start: date,
           end: date
       },
       ...properties
-      }];
-      let files = await getPaths(data, false);
-      reqData.sizeF = files.numFiles;
-      let file = "";
-      //should only be exactly one file
-      if(files.numFiles == 0 && returnEmptyNotFound) {
+    }];
+    let files = await getPaths(data, false);
+    reqData.sizeF = files.numFiles;
+    let file = "";
+    //should only be exactly one file
+    if(files.numFiles == 0 && returnEmptyNotFound) {
       file = getEmpty(properties.extent);
-      }
-      else {
+    }
+    else {
       file = files.paths[0];
-      }
-      
-      if(!file) {
+    }
+    
+    if(!file) {
       //set failure and code in status
       reqData.success = false;
       reqData.code = 404;
@@ -162,14 +163,14 @@ router.get("/raster", async (req, res) => {
       //resources not found
       res.status(404)
       .send("The requested file could not be found");
-      }
-      else {
+    }
+    else {
       reqData.code = 200;
       res.status(200)
       .sendFile(file);
-      }
+    }
   });
-  });
+});
 
 
 router.get("/production/list", async (req, res) => {
@@ -236,3 +237,135 @@ router.get("/stations", async (req, res) => {
     }
   });
 });
+
+
+router.get("/production/list", async (req, res) => {
+  const permission = "basic";
+  await handleReq(req, res, permission, async (reqData) => {
+      let data: any = req.query.data;
+      data = JSON.parse(data);
+      if(!Array.isArray(data)) {
+        //set failure and code in status
+        reqData.success = false;
+        reqData.code = 400;
+
+        res.status(400)
+        .send(
+            "Request must include the following parameters: \n\
+            data: A JSON object representing the dataset to be listed."
+        );
+      }
+      else {
+      let files = await getPaths(data, false);
+      reqData.sizeF = files.numFiles;
+      let fileLinks = files.paths.map((file) => {
+          file = path.relative(dataRoot, file);
+          let fileLink = `${urlRoot}${file}`;
+          return fileLink;
+      });
+      reqData.code = 200;
+      res.status(200)
+      .json(fileLinks);
+      }
+  });
+});
+
+router.get("/files/explore/*", async (req, res) => {
+  const permission = "basic";
+  await handleReq(req, res, permission, async (reqData) => {
+    const allowedDirs = ["NASA_downscaling", "production", "workflow_data", "raw", "backup_data_aqs"]
+    const allowedPaths = allowedDirs.map((sub: string) => path.join(dataPath, sub));
+    const epBase = url.resolve(apiURL, "/files/explorer")
+    const userPath = req.params[0];
+    const dataPath = path.resolve(path.join(dataRoot, userPath));
+    if(dataPath == dataRoot) {
+      const pathData: FileData[] = allowedDirs.map((dir: string) => {
+        return {
+          url: url.resolve(epBase, dir),
+          name: dir,
+          path: dir,
+          sizeBytes: 4096,
+          ext: "",
+          type: "d"
+        }
+      });
+      reqData.code = 200;
+      return res.status(200)
+      .json(pathData);
+    }
+    let allowed = false;
+    for(let root in allowedPaths) {
+      let rel = path.relative(root, dataPath);
+      //if relative path is equivalent to one of the allowed paths or it is a subfolder (no .. and relative), then path is allowed
+      if(rel.length == 0 || (!rel.startsWith("..") && !path.isAbsolute(rel))) {
+        allowed = true;
+        break;
+      }
+    }
+    
+    //if path is not allowed or does not exist return 404
+    if(!(allowed && fs.existsSync(dataPath))) {
+      //set failure and code in status
+      reqData.success = false;
+      reqData.code = 404;
+
+      return res.status(404)
+      .send("The requested file does not exist or is not accessible.");
+    }
+
+    let stat = fs.lstatSync(dataPath);
+    if(stat.isFile()) {
+      reqData.code = 200;
+      return res.status(200)
+      .sendFile(dataPath);
+    }
+    else if(stat.isDirectory()) {
+      let data = fs.readdirSync(dataPath)
+      .reduce((pathData: FileData[], file: string) => {
+        let fpath = path.join(dataPath, file);
+        let subStat = fs.lstatSync(fpath);
+        if(subStat.isFile()) {
+          pathData.push({
+            url: url.resolve(epBase, fpath),
+            name: file,
+            path: fpath,
+            sizeBytes: subStat.size,
+            ext: path.extname(file),
+            type: "f"
+          });
+        }
+        else if(subStat.isDirectory()) {
+          pathData.push({
+            url: url.resolve(epBase, fpath),
+            name: file,
+            path: fpath,
+            sizeBytes: subStat.size,
+            ext: "",
+            type: "d"
+          });
+        }
+        return pathData;
+      }, []);
+      reqData.code = 200;
+      return res.status(200)
+      .json(data);
+    }
+    else {
+      //set failure and code in status
+      reqData.success = false;
+      reqData.code = 404;
+
+      return res.status(404)
+      .send("The requested file does not exist or is not accessible.");
+    }
+  });
+});
+
+interface FileData {
+  url: string,
+  path: string,
+  name: string,
+  ext: string,
+  sizeBytes: number,
+  type: "f" | "d"
+}
