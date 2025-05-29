6a757009d1c18da3dc33e6fdbbe0fc58e2858b45
jardmclan <mcleanj@hawaii.edu>
2025-05-28 15:53:55 -1000

    improve health check, add second monitor endpoint

--- src/app/endpoints/util/health.ts
+++ src/app/endpoints/util/health.ts
@@ -5,7 +5,7 @@ import { readFileSync } from "fs";
 
 export const router = express.Router();
 
-router.get("/health", async (req, res) => {
+async function checkHealth() {
   const health = {
     apiDB: false,
     mesonetDBUser: false,
@@ -36,6 +36,27 @@ router.get("/health", async (req, res) => {
     health.fs = true;
   }
   catch(e) {}
+  return health;
+}
+
+router.get("/healthmonitor", async (req, res) => {
+  let health = await checkHealth();
   return res.status(200)
   .json(health);
+});
+
+router.get("/health", async (req, res) => {
+  let health = await checkHealth();
+  let isHealthy = true;
+  for(let item in health) {
+    isHealthy &&= health[item];
+  }
+  if(isHealthy) {
+    return res.status(204)
+    .end();
+  }
+  else {
+    return res.status(503)
+    .send("Some of the services the API relies on are unreachable or not available at this time. Some parts of the API may not function as expected until this is resolved.");
+  }
 });
