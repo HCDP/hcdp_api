bf357f8bc48da442f6541955044fdbb1c8de1fe6
jardmclan <mcleanj@hawaii.edu>
2025-05-29 17:25:44 -1000

    update deploy scripts

--- util/deploy_test.sh
+++ util/deploy_test.sh
@@ -1,15 +1,4 @@
 #!/bin/bash
 
-docker stop apitest
-docker wait apitest
-docker rm apitest
-
-cp -R ../api/certs/live certs
-cp -R ../api/certs/archive certs
-
-docker build -t hcdp_api_test .
-
-docker run --name=apitest -d -p 8443:443 \
--v /mnt/lustre/annotated/HCDP:/data \
--v /home/hcdp/hcdp-api/logs:/logs \
-hcdp_api_test
+docker compose --profile dev build
+docker compose --profile dev up -d
