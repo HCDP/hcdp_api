d5375be1a8e199c93bdd50b5c1e1aba791801cd2
jardmclan <mcleanj@hawaii.edu>
2025-08-08 17:27:55 -1000

    debug restart child

--- src/app/endpoints/admin/misc/misc.ts
+++ src/app/endpoints/admin/misc/misc.ts
@@ -275,6 +275,6 @@ router.post("/restart", async (req, res) => {
     reqData.code = 202;
     res.status(202)
     .send("Request received. API will attempt to restart");
-    child_process.spawn("bash docker stop api; bash /home/hcdp/hcdp-api/api/util/deploy.sh");
+    child_process.spawn("bash echo test; bash echo a");
   });
 });
