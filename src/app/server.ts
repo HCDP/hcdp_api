0abcfeed68bf84ee15fa8bc8ea82b066b9e2fd25
jardmclan <mcleanj@hawaii.edu>
2026-03-30 17:51:12 -1000

    increase max payload size only for insert endpoint

--- src/app/server.ts
+++ src/app/server.ts
@@ -66,8 +66,7 @@ app.use((req, res, next) => {
 // // Apply the rate limiting middleware to all requests.
 // app.use(cutoffLimit);
 
-app.use(express.json({ limit: '50mb' }));
-app.use(express.urlencoded({ limit: '50mb', extended: true }));
+app.use(express.json());
 
 const server = app.listen(port, () => {
   console.log("Server running on port " + port);
