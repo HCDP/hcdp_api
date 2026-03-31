a69fe14eac4d8f4a50912f4e7d9b2fcc1c148884
jardmclan <mcleanj@hawaii.edu>
2026-03-30 17:48:01 -1000

    increase max payload size

--- src/app/server.ts
+++ src/app/server.ts
@@ -66,7 +66,8 @@ app.use((req, res, next) => {
 // // Apply the rate limiting middleware to all requests.
 // app.use(cutoffLimit);
 
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 const server = app.listen(port, () => {
   console.log("Server running on port " + port);
